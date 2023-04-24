89ff843bb6135aa4315e380a3e8fbf90a7349554
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-24 13:45:50 +0530

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -1,22 +1,3 @@
-data "aws_iam_policy_document" "kms_policy" {
-  statement {
-    sid    = "Enable IAM User Permissions"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "kms:*"
-    ]
-
-    resources = [
-      aws_kms_key.kms_key.arn
-    ]
-  }
-}
 
 resource "aws_kms_key" "kms_key" {
   description             = "KMS key for encrypting sensitive data"
