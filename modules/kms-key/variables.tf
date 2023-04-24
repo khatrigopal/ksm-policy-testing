2199e0dcbb8f1b87f8baacc020c3d7241473b240
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-24 13:31:04 +0530

    Create variables.tf

---
+++ modules/kms-key/variables.tf
@@ -0,0 +1,9 @@
+variable "key_description" {
+  type        = string
+  description = "The description for the KMS key"
+}
+
+variable "alias_name" {
+  type        = string
+  description = "The name for the KMS key alias"
+}
