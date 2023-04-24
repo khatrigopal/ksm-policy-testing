b23a4227bfa3da94ee0d5877697f09a156a4b65c
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-04-24 13:43:59 +0530

    Update main.tf

--- modules/kms-key/main.tf
+++ modules/kms-key/main.tf
@@ -1,10 +1,3 @@
-resource "aws_kms_key" "kms_key" {
-  description             = "KMS key for encrypting sensitive data"
-  enable_key_rotation     = true
-  policy                  = data.aws_iam_policy_document.kms_policy.json
-  deletion_window_in_days = 7
-}
-
 data "aws_iam_policy_document" "kms_policy" {
   statement {
     sid    = "Enable IAM User Permissions"
@@ -24,6 +17,15 @@ data "aws_iam_policy_document" "kms_policy" {
     ]
   }
 }
+
+resource "aws_kms_key" "kms_key" {
+  description             = "KMS key for encrypting sensitive data"
+  enable_key_rotation     = true
+  policy                  = data.aws_iam_policy_document.kms_policy.json
+  deletion_window_in_days = 7
+}
+
+
 
 resource "aws_kms_alias" "kms_key_alias" {
   name          = var.key_alias
